716171f0ebf9b09e9b15ee1e7bddadc2cfacff5b
anjos-gabriel <anjos.gabriel999@gmail.com>
2022-01-07 14:45:29 -0300

    grid layout

--- src/components/Hashtag/index.js
+++ src/components/Hashtag/index.js
@@ -1,22 +1,24 @@
 import React from "react";
+import './styles.css';
 import Card from "../../objects/Card";
 import Player from "../../objects/Player";
 
-const Hashtag=()=>{
-    return(
-        <Card>
-        <Player player="x"/>
-        <Player player="o"/>
-        <Player player="x"/>
-        
-        <Player player="o"/>
-        <Player player="x"/>
-        <Player player="x"/>
-  
-        <Player player="x"/>
-        <Player player="o"/>
-        <Player player="o"/>
-        </Card>
-    );
-}
+const Hashtag = () => {
+  return (
+    <Card>
+      <ul className="hashtag">
+        <li className="item"><Player player="x"/></li>
+        <li className="item"><Player player="x"/></li>
+        <li className="item"><Player player="o"/></li>
+        <li className="item"><Player player="x"/></li>
+        <li className="item"><Player player="o"/></li>
+        <li className="item"><Player player="x"/></li>
+        <li className="item"><Player player="o"/></li>
+        <li className="item"><Player player="x"/></li>
+        <li className="item"><Player player="x"/></li>
+       
+      </ul>
+    </Card>
+  );
+};
 export default Hashtag;
