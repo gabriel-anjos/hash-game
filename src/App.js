a2466fb6b0555168b63a97a3f6b82146a30b94d2
anjos-gabriel <anjos.gabriel999@gmail.com>
2022-01-07 14:19:13 -0300

    hashtag component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React,{Fragment}from 'react';
-import Card from './objects/Card';
+import Hashtag from './components/Hashtag';
 import Header from './components/Header';
-import Player from './objects/Player';
 function App() {
   return(
     //ou pode usar <>  para nao precisar escrever fragment nem importalo.
     <Fragment>
       <Header/>
-      <Card>
-      <Player player="x"/>
-      <Player player="o"/>
-      <Player player="x"/>
-      
-      <Player player="o"/>
-      <Player player="x"/>
-      <Player player="x"/>
-
-      <Player player="x"/>
-      <Player player="o"/>
-      <Player player="o"/>
-      </Card>
+      <Hashtag/>
     </Fragment>
     //precisa </> fecha-lo
 
---
+++ src/components/Hashtag/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Card from "../../objects/Card";
+import Player from "../../objects/Player";
+
+const Hashtag=()=>{
+    return(
+        <Card>
+        <Player player="x"/>
+        <Player player="o"/>
+        <Player player="x"/>
+        
+        <Player player="o"/>
+        <Player player="x"/>
+        <Player player="x"/>
+  
+        <Player player="x"/>
+        <Player player="o"/>
+        <Player player="o"/>
+        </Card>
+    );
+}
+export default Hashtag;
