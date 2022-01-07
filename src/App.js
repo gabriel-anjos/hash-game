458e5bad28d551dbe04bd1b15a8808092d65f998
anjos-gabriel <anjos.gabriel999@gmail.com>
2022-01-07 12:55:37 -0300

    object card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React,{Fragment}from 'react';
+import Card from './objects/Card';
 import Header from './components/Header';
 function App() {
   return(
     //ou pode usar <>  para nao precisar escrever fragment nem importalo.
     <Fragment>
       <Header/>
+      <Card/>
     </Fragment>
     //precisa </> fecha-lo
 
---
+++ src/objects/Card/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+// import './styles.css';
+import './styles.css'
+const Card=()=>{
+    return(<article className="card"></article>)
+}
+export default Card;
