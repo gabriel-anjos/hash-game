4ff2cab750603209dde24918f82a2c7b2822a971
anjos-gabriel <anjos.gabriel999@gmail.com>
2022-01-07 15:22:38 -0300

    object label

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React,{Fragment}from 'react';
 import './App.css';
 import Hashtag from './components/Hashtag';
 import Header from './components/Header';
+import Label from './objects/Label';
 function App() {
   return(
     //ou pode usar <>  para nao precisar escrever fragment nem importalo.
@@ -9,7 +10,7 @@ function App() {
       <main className='app'>
       <Header/>
       <Hashtag/>
-
+      <Label content='mostrar eventos'/>
       </main>
     
     //precisa </> fecha-lo
