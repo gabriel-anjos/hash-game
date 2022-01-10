4abc90130d730ae15c66aa3f2e69b64996e1e7bb
anjos-gabriel <anjos.gabriel999@gmail.com>
2022-01-10 18:52:51 -0300

    iconSocial object created

--- src/components/ProfileUser/index.js
+++ src/components/ProfileUser/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import './styles.css';
 import profile from'../../img/profile.jpg';
+import GitHub from '../../img/git-icon.png';
+import Linkedin from '../../img/linkedin-icon.png';
+import Facebook from '../../img/facebook-icon.png';
+import Twitter from '../../img/twitter-icon.png';
+import Instagram from '../../img/instagram-icon.png';
 import AvatarProfile from "../../objects/AvatarProfile";
+import IconSocial from "../../objects/IconSocial";
 
 const ProfileUser =()=>{
     return(
@@ -11,6 +17,13 @@ const ProfileUser =()=>{
             </dd>
             <dt className="title">Gabriel anjos</dt>
             <dd className="description">Em processo de aprendizado e aperfeiçoamentos em tecnologias de desenvolvimento de softwares</dd>
+            <dd className="socials">
+                <IconSocial src={GitHub} alt="logo da Github"/>
+                <IconSocial src={Linkedin} alt="logo do linkedin"/>
+                <IconSocial src={Facebook} alt="logo do facebook"/>
+                <IconSocial src={Twitter} alt="logo do twitter"/>
+                <IconSocial src={Instagram} alt="logo do instagram"/>
+            </dd>
         </dl>
     );
 }
---
+++ src/objects/IconSocial/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+const IconSocial=({src,alt})=>{
+    return(<img className="IconSocial" src={src} alt={alt}></img>)
+}
+export default IconSocial;
