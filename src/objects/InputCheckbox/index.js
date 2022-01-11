f999fbad8d4328e01dc53360d06a293a1fdabc78
anjos-gabriel <anjos.gabriel999@gmail.com>
2022-01-11 14:37:57 -0300

    envent click labelGame ande inputCheckbox

--- src/objects/InputCheckbox/index.js
+++ src/objects/InputCheckbox/index.js
@@ -2,11 +2,15 @@ import React from "react";
 import './styles.css'
 import LabelGame from "../LabelGame";
 import InputGame from "../inputGame";
+
+const handleClick=()=>{
+  console.log('aeaee')
+}
 const InputCheckbox=({id='',value='',content=''})=>{
     return(
         <>
         <InputGame id={id}value={value} type="checkbox"content={content}/>
-        <LabelGame  htmlFor={id} content={content} />
+        <LabelGame onClick={handleClick} htmlFor={id} content={content} />
       </>
     );
 }
--- src/objects/LabelGame/index.js
+++ src/objects/LabelGame/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './styles.css'
-const LabelGame=({htmlFor,content})=>{
-    return(<label className="label-game"htmlFor={htmlFor}>{content}</label>)
+const LabelGame=({htmlFor,content,onClick})=>{
+    return(<label onClick={onClick}className="label-game"htmlFor={htmlFor}>{content}</label>)
 };
 
 export default LabelGame;
