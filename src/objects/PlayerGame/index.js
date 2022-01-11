7632520242121e8c6487ac5a6fe87ed37db0e865
anjos-gabriel <anjos.gabriel999@gmail.com>
2022-01-11 12:30:39 -0300

    refactoring Player to PlayerGame

--- src/objects/PlayerGame/index.js
+++ src/objects/PlayerGame/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import './styles.css';
 import PlayerX from '../../img/X.png';
 import PlayerO from '../../img/Circle.png'
-const Player=({player})=>{
+const PlayerGame=({player})=>{
    const players=[];
    players['x']=PlayerX
    players['o']=PlayerO
@@ -13,6 +13,6 @@ const Player=({player})=>{
     // }else if(props.player==='o'){
     //     player=PlayerO;
     // }
-    return(<button className="player"><img src={players[player]} alt={`jogador ${player}`}></img></button>)
+    return(<button className="player-game"><img src={players[player]} alt={`jogador ${player}`}></img></button>)
 }
-export default Player;
+export default PlayerGame;
