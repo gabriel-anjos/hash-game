8d5fa6b421fda91cc9bfd3ed39e4f43e1e4e54ab
anjos-gabriel <anjos.gabriel999@gmail.com>
2022-01-11 13:17:51 -0300

    TagGame css

--- src/objects/TagGame/index.js
+++ src/objects/TagGame/index.js
@@ -1,5 +1,6 @@
 import React from "react";
+import './styles.css';
 const TagGame=({content})=>{
-    return(<output className="tag-name">{content}</output>)
+    return(<output className="tag-game">{content}</output>)
 }
 export default TagGame;
