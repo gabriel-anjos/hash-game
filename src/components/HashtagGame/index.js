d9ae0ad89b230d80541a31c09de392e45c8f74e8
anjos-gabriel <anjos.gabriel999@gmail.com>
2022-01-12 12:13:49 -0300

    x and o onClick

--- src/components/HashtagGame/index.js
+++ src/components/HashtagGame/index.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import './styles.css';
 import CardGame from "../../objects/CardGame";
 import PlayerGame from "../../objects/PlayerGame";
 
 const HashtagGame = () => {
+  const [nextPlayer,setNextPlayer]= useState("x");
+  const handleClick=()=>{
+      console.log("proximo jogador",nextPlayer);
+      setNextPlayer((old)=> old === "x" ? "o" : "x");
+
+      // setNextPlayer((old)=>{
+      //   if(old==="x"){
+      //     return "o";
+      //   }else{
+      //   return "x"
+      //   }
+      // })
+  }
   return (
     <CardGame>
-      <ul className="hashtag-game">
+      <ul className="hashtag-game" onClick={handleClick}>
         <li className="item"><PlayerGame/></li>
         <li className="item"><PlayerGame/></li>
         <li className="item"><PlayerGame/></li>
