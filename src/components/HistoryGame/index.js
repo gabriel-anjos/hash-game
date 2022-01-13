777fda753b1fd0de34639ec0595941d2161a5dda
anjos-gabriel <anjos.gabriel999@gmail.com>
2022-01-13 15:21:41 -0300

    click event on historygame

--- src/components/HistoryGame/index.js
+++ src/components/HistoryGame/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import TagGame from "../../objects/TagGame";
 import './styles.css';
+
+const changeHistory=()=>{
+    console.log("#@3##")
+}
 const HistoryGame =({history})=>(
     
         <ol className={"history-game"}>
             {history.map((content,key)=>( 
-            <li key={key} className="action">
+            <li key={key} className="action" onClick={changeHistory}>
                 <TagGame content={content}/>
             </li>
             ))}
